a8b247ad1ebf3b16f85738bcb7f8fbe069ef1b6f
Watcharin <twatcharin90@gmail.com>
2021-09-23 16:47:23 +0700

    19.

--- es6/18.ArrayReduce.js
+++ es6/18.ArrayReduce.js
@@ -1,16 +1,31 @@
 //ลดข้อมูลใน array
 
-const data = [10, 20, 30, 40];
-const mapData = data.map(e=> 100)  // เปลียนเป็น 100
-console.log(mapData)
-const filterData = data.filter(e=> e>20)
-console.log(filterData)
+// const data = [10, 20, 30, 40];
+// const mapData = data.map(e=> 100)  // เปลียนเป็น 100
+// console.log(mapData)
+// const filterData = data.filter(e=> e>20)
+// console.log(filterData)
 
-//////array.reduce((ค่าที่ถูกประมวลผม, e)=>{},ค่าเริมต้น)
+// //////array.reduce((ค่าที่ถูกประมวลผม, e)=>{},ค่าเริมต้น)
 
-result=data.reduce((value,e)=>{
-    // console.log(value)
-    const total = e+value;
-    return total
-},0)
-console.log(result)
+// result=data.reduce((value,e)=>{
+//     // console.log(value)
+//     const total = e+value;
+//     return total
+// },0)
+// console.log(result)
+
+////////////////////////////////////////////////////////////////
+
+const cart = [
+  { name: "กระเป๋า", price: 500 },
+  { name: "หนังสือ", price: 900 },
+  { name: "กล้อง", price: 5000 },
+];
+
+const summation = cart.reduce((value, e) => {
+  total = e.price + value;
+  return total;
+}, 0);
+
+console.log(summation)
