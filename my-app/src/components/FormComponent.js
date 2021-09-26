1f405822b60675d8a2d0ba4670667a7ec7026b79
Watcharin <twatcharin90@gmail.com>
2021-09-26 13:54:45 +0700

    39.event

--- my-app/src/components/FormComponent.js
+++ my-app/src/components/FormComponent.js
@@ -2,16 +2,34 @@ import React from "react";
 import "./FormComponent.css";
 
 const FormComponent = () => {
+  const inputTitle = (event) => {
+    console.log(event.target.value);
+  };
+  const inputAmount = (event) => {
+    console.log(event.target.value);
+  };
+  const saveItem = (event) => {
+    event.preventDefault(); //ไม่ให้จอ refresh
+    console.log("บันทึกข้อมูลเรียบร้อย");
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={saveItem}>
         <div className="form-control">
           <label>ชื่อรายการ</label>
-          <input type="text" placeholder="ระบุชื่อรายการของคุณ"></input>
+          <input
+            type="text"
+            placeholder="ระบุชื่อรายการของคุณ"
+            onChange={inputTitle}
+          />
         </div>
         <div className="form-control">
           <label>จำนสนเงิน</label>
-          <input type="number" placeholder="(+ รายรับ , - รายจ่าย)"></input>
+          <input
+            type="number"
+            placeholder="(+ รายรับ , - รายจ่าย)"
+            onChange={inputAmount}
+          />
         </div>
         <div>
           <button type="submit" className="btn ">
