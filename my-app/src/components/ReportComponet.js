58e5d366ea81c6f3df8b40fd67e31c02698cddd8
Watcharin <twatcharin90@gmail.com>
2021-09-30 23:15:35 +0700

    50.reportcomponent

--- my-app/src/components/ReportComponet.js
+++ my-app/src/components/ReportComponet.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import DataContext from "../data/DataContext";
+import "./RepoerComponent.css";
 
 const ReportComponet = () => {
   // const name = useContext(DataContext);
@@ -8,9 +9,22 @@ const ReportComponet = () => {
   const { income, expense } = useContext(DataContext);
   return (
     <div>
+      <h4>ยอดคงเหลือ (บาท)</h4>
+      <h1>{income - expense}</h1>
+      <div className="report-container">
+        <div>
+          <h4>ยอดรายรับ</h4>
+          <p className="report plus">{income}</p>
+        </div>
+        <div>
+          <h4>ยอดรายจ่าย</h4>
+          <p className="report minus">{expense}</p>
+        </div>
+      </div>
+
       {/* //วิธีที่ 2 useContext */}
-      <p>รายรับ {income}</p>
-      <p>รายจ่าย {expense}</p>
+      {/* <p>รายรับ {income}</p>
+      <p>รายจ่าย {expense}</p> */}
 
       {/* วิธีที่ 1.ใช่ข้อมูลโดย consumer */}
       {/* <DataContext.Consumer>
